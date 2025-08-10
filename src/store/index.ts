19872676ea85aceb88ae7b7f64213b0b0c3bbf28
vladimirtugutov <vladimir.tugutov@gmail.com>
2025-08-10 16:33:17 -0300

    feat: update store config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import selectedItemsReducer from './selectedItemsSlice';
+import { booksApi } from './api/booksApi';
 
 export const store = configureStore({
   reducer: {
     selectedItems: selectedItemsReducer,
+    [booksApi.reducerPath]: booksApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(booksApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
