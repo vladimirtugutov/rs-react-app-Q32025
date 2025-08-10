b7a590b1f58da1df0580a4af93b90a7c7218a431
vladimirtugutov <vladimir.tugutov@gmail.com>
2025-08-09 21:43:58 -0300

    refactor: apply mentor's notes

--- src/utils/isArrayWithItems.ts
+++ src/utils/isArrayWithItems.ts
@@ -1,3 +1,2 @@
-export function isArrayWithItems<T>(arr: unknown): arr is T[] {
-  return Array.isArray(arr) && arr.length > 0;
-}
+export const isArrayWithItems = <T>(arr: unknown): arr is T[] =>
+  Array.isArray(arr) && arr.length > 0;
