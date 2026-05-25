ad42ae7320fb1ac9994069429b003877daac5710
Vladimir Tugutov <tugutov@gmail.com>
2026-05-25 19:39:55 -0300

    fix: fix tests

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { App } from './App';
-import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
+import { ErrorBoundary } from '../components/ErrorBoundary/ErrorBoundary';
 import selectedItemsReducer from '../store/selectedItemsSlice';
 
 const mockFetch = vi.fn();
